c321d6867dade0bb06c33486a16a79948987c240
Rohit <sketchlearn7@gmail.com>
2020-02-26 18:17:14 +0530

    first commit

---
+++ PhotoBook/ViewModel/CollectionViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  CollectionViewModel.swift
+//  PhotoBook
+//
+//  Created by MACBOOK on 26/02/20.
+//  Copyright © 2020 Saini. All rights reserved.
+//
+
+import Foundation
---
+++ PhotoBookTests/Mocks/MockCollectionViewController.swift
@@ -0,0 +1,9 @@
+//
+//  MockCollectionViewController.swift
+//  PhotoBookTests
+//
+//  Created by MACBOOK on 26/02/20.
+//  Copyright © 2020 Saini. All rights reserved.
+//
+
+import Foundation
